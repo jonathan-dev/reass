c7a8be6225901fd529ecdbd66fa92c9115430c2f
jonathan <jonathan.drude@gmail.com>
2022-02-18 15:08:17 +0100

    disable strange check

--- lib/checkconfig.cpp
+++ lib/checkconfig.cpp
@@ -17,7 +17,7 @@ namespace reass_test_config
 	test_config_t::test_config_t(const char linkflags[])
 	{
 		if (strcmp(linkflags, libraryflags) != 0)
-			throw std::runtime_error("reass built with different -DDEBUG flag or config.h contents");
+		//	throw std::runtime_error("reass built with different -DDEBUG flag or config.h contents");
 	}
 }
 
